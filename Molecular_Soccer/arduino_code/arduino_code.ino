314f150963b4562b215a16df8820c2aac2300c09
ALanLechengChao <lechengchao@163.com>
2021-06-13 16:47:01 +0800

    test audio

--- Molecular_Soccer/arduino_code/arduino_code.ino
+++ Molecular_Soccer/arduino_code/arduino_code.ino
@@ -20,14 +20,7 @@ void loop() {
       }
     
   }
-
-  // keep this format
-//  Serial.print(sensor1);
-//  Serial.print(",");  // put comma between sensor values
-//  Serial.print(sensor2);
-//  Serial.print(",");
-//  Serial.print(sensor3);
-    Serial.println(); // add linefeed after sending the last sensor value
+  Serial.println(); // add linefeed after sending the last sensor value
 
   // too fast communication might cause some latency in Processing
   // this delay resolves the issue.
